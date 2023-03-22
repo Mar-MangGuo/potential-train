ed9abd63412a76a9204435e57c2b3e998f8871c7
Mar-MangGuo <128587623+Mar-MangGuo@users.noreply.github.com>
2023-03-22 17:35:26 +0800

    Add files via upload

---
+++ test 3_21.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main()
+/*{
+    int input=0;
+    printf("好好学习\n");
+    printf("有没有好好学习(1/0)>:");
+    scanf("%d",  &input);
+    if (input==1)
+        printf("成功\n");
+        else
+        printf("卖红薯\n");
+    
+
+    return 0;
+
+    int line=0;
+    printf("敲一行代码\n");
+
+    while(line<2)
+    {
+        printf("敲一行代码  目前是第%d行\n" ,line+1);
+        line++;
+    }
+     if(line>=2)
+    printf("成功");
+    return 0;
+}
+int ADD(int x,int y)
+{
+    int z=x+y;
+    return z;
+}
+{int a,b,c=1,2,3;
+int sum=0;
+sum=ADD(a,b);
+    printf("sum=%d\n",sum);
+    return 0;
+
+
+}*/
+{
+    
+    return 0;
+}
