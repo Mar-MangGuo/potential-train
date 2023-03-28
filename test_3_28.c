b9c0cf0433162af5797ed4c6849e5f20a23f9cdb
Qingqingzijin-why <128587623+Qingqingzijin-why@users.noreply.github.com>
2023-03-28 17:36:08 +0800

    Add files via upload

---
+++ test_3_28.c
@@ -0,0 +1,113 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	float c = 0;
+//	scanf("%d%d", &a,&b);
+//	c = a / b;
+//	int d = a % b;
+//	printf("%f  %d", c,d);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = -1;
+//	big int b=a >> 1;
+//	printf("%d", b);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 5;
+//	int b = a << 2;
+//	printf("%d", b);
+//
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	int c = a ^ b;
+//	printf("%d", c);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	/*a = a + b;
+//	b = a - b;
+//	a = a - b;
+//	printf("a=%d,b=%d", a, b);*/
+//
+//	a = a ^ b;
+//	b = a ^ b;
+//	a = a ^ b;
+//	printf("a=%d,b=%d", a, b);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int num = 0;
+//	int count = 0;
+//	scanf("%d", &num);
+//
+//	int i = 0;
+//	for ( i = 0; i < 32; i++)
+//	{
+//		if (1 == ((num >> i) & 1))
+//			count++;
+//	}
+//
+//	/*while (num)
+//	{
+//		if (num%2==0)
+//		{
+//			count++; 
+//			num = num / 2;
+//		}
+//	}*/
+//	printf("%d\n", count);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	a = a + 2;
+//	a += 2;
+//	a = a >> 2;
+//	a=a
+//
+//}
+
+int main()
+{
+	int a = 0;
+	if (a)
+		printf("hehe");
+	else if (!a)
+		printf("haha");
+
+	return 0;
+}
+
+//int main()
+//{
+//	int a = 10;
+//	int* p = &a;
+//	*p = 20;
+//
+//	return 0;
+//}
+
